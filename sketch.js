c5a03513a3e08017e76a40a1fce2f01c8b7bc095
rabisc0 <cruzdavi348@gmail.com>
2024-06-14 08:22:17 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(600,600);
+    background("black") 
+   }
+  function draw(){ 
+  stroke("red");
+  fill("green");
+    if(mouseIsPressed){
+      rect(mouseX,mouseY,20,35)
+  }
+  }
